7dfe361854626f897cc2361c31522adc8e16b945
MJUDDIN01 <mjuddin88@gmail.com>
2023-09-12 12:38:00 +0100

    Initial Commit

---
+++ AppInSwiftUI/AppInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppInSwiftUIApp.swift
+//  AppInSwiftUI
+//
+//  Created by Jasim Uddin on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
