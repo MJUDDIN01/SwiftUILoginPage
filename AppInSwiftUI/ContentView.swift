d694fc359096332fdf58c14d612a243de16c6577
MJUDDIN01 <mjuddin88@gmail.com>
2023-09-18 14:19:49 +0100

    Added simple login UI

--- AppInSwiftUI/ContentView.swift
+++ AppInSwiftUI/ContentView.swift
@@ -8,19 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var email = ""
+    @State var password = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(spacing: 15) {
+                TextField("E-mail", text: self.$email)
+                    .autocapitalization(.none).textContentType(.emailAddress)
+                Rectangle().frame(height: 1).foregroundColor(Color.gray)
+                SecureField("Password", text: self.$password).padding(.top, 10)
+                Rectangle().frame(height: 1).foregroundColor(Color.gray)
+                Button {
+                    // No action yet
+                } label: {
+                    Text("Sign In")
+                        .padding()
+                        .foregroundColor(Color.white)
+                        .frame(width: 300, height: 50)
+                        .background(Color.indigo)
+                        .cornerRadius(25)
+                }
+                Text("Forgot the password?")
+
+                                }
+            .padding(.leading, 10)
+            .navigationTitle("Sign In")
         }
-        .padding()
+        VStack {
+            HStack {
+                Text("Don't have account?")
+                Link("Create here", destination: URL(string:"https://www.google.co.uk/")!)
+            }
+            
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
